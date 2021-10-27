fd1c1dade38a745b0b9f6b4f15da50a7a5ea0f56
David Essien <davidessienshare@gmail.com>
2021-10-27 12:32:05 +0100

    add more widgets to the container

--- first_app/main.go
+++ first_app/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -13,13 +15,27 @@ func main() {
 
 	a := app.New()
 	w := a.NewWindow("Welcome to fyne")
-
 	hello := widget.NewLabel("Hello fyne")
+	checkBox := widget.NewCheck("Is this morning?", func(b bool) {
+		if b == true {
+			hello.SetText("Good morning!!!")
+		} else {
+			hello.SetText("Good day!!!")
+		}
+	})
+	btn := widget.NewButton("Hi!", func() {
+		hello.SetText("Welcome :")
+	})
+
+	url, _ := url.Parse("https://github.com/davidshare")
+	hyperlink := widget.NewHyperlink("See my github profile", url)
+
 	w.SetContent(container.NewVBox(
 		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :")
-		}),
+		checkBox,
+		btn,
+		hyperlink,
 	))
+	w.Resize(fyne.NewSize(200, 400))
 	w.ShowAndRun()
 }
