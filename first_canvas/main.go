ff6cf95a761e34fc3496bcb99d54a686f5d3847f
David Essien <davidessienshare@gmail.com>
2021-10-27 13:09:46 +0100

    add text canvas

---
+++ first_canvas/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+)
+
+func main() {
+	canvasApp := app.New()
+
+	w := canvasApp.NewWindow("My Canvas Text tutorial")
+	w.Resize(fyne.NewSize(400, 400))
+	colorX := color.NRGBA{R: 0, G: 255, B: 0, A: 255}
+	textX := canvas.NewText("This is the text", colorX)
+	img := canvas.NewImageFromFile("./47.jpg")
+	w.SetContent(textX)
+	w.ShowAndRun()
+}
